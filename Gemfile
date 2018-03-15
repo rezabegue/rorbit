9a0f3669457d9d9c9cde4c4638918fffb1b35514
Selim Reza <selimrezael@gmail.com>
2018-03-16 00:24:36 +0600

    user authentication with devise

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'figaro'
 gem 'jquery-rails'
+gem 'devise', '~> 4.4', '>= 4.4.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "articles#index"
+  root to: "articles#index"
 end
